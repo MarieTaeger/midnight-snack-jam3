0c70307133664ad6df3f5deccd313b764550d94a
Marie Taeger <61857823+MarieTaeger@users.noreply.github.com>
2020-03-17 13:17:32 +1300

    Sandwich pickup

---
+++ Assets/Scripts/SCR_SandwichCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_SandwichCollider : MonoBehaviour
+{
+
+    public GameObject Mouth;
+    public GameObject TestSandwich;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //should attach the Sandwich to the raccoon upon collision
+        Debug.Log("Sandwich detected");
+        TestSandwich.GetComponent<FixedJoint>().connectedBody = Mouth.GetComponent<Rigidbody>();
+    }
+}
